ff00b8326809a7989a2cafb5412655038a1d6ee6
Jose Manuel Requena Plens <jmrplens@gmail.com>
2025-12-21 00:24:46 +0100

    fix: convert lighthouse script to ESM

--- scripts/format-lighthouse-report.js
+++ scripts/format-lighthouse-report.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
 
 const manifestPath = "./.lighthouseci/manifest.json";
 const linksPath = "./.lighthouseci/links.json";
